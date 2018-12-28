13826280b142cd94bbf208b5ab0fbfd77f544a65
Hakamir <hakamir@localhost.localdomain>
2018-12-28 15:53:40 +0100

    Committer: Hakamir <hakamir@localhost.localdomain>
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	deleted:    wifibot/client.cpp
    	deleted:    wifibot/client.h
    	modified:   wifibot/main.cpp
    	deleted:    wifibot/server.cpp
    	deleted:    wifibot/server.h
    
     Untracked files:
    	wifibot/WifibotClient.cpp
    	wifibot/WifibotClient.h
    	wifibot/WifibotClient.o
    	wifibot/main.o
    	wifibot/makefile
    	wifibot/prog

--- wifibot/main.cpp
+++ wifibot/main.cpp
@@ -1,15 +1,73 @@
-#include "client.h"
+#include "WifibotClient.h"
 
+#include <ncurses.h>
+#include <unistd.h>
+
+#define IP_ADDRESS "192.168.1.182"
+#define CONTROL_PORT 15020
+#define VIDEO_PORT 8080
 
 WifibotClient robot;
 
-void main(void)
+int kbhit(void) {
+	int ch = getch();
+
+	if (ch != ERR) {
+		ungetch(ch);
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int main(void)
 {
-	err = robot.ConnectToRobot(IP_ADDRESS, CONTROL_PORT);
+	//initscr();
+	//cbreak();
+	//noecho();
+	//nodelay(stdscr, TRUE);
+
+	bool err;
+	err = robot.ConnectToRobot((char*)IP_ADDRESS, CONTROL_PORT);
 	if (err == true){
-		printf("Connection established.");
+		printf("Connection established.\n");
 	}
 	else{
-		printf("ERROR Connection failed.");
+		printf("ERROR Connection failed.\n");
 	}
+	while(1){
+		robot.avancer(100,100);
+		usleep(1000000);
+		robot.reculer(100,100);
+		usleep(1000000);
+	}
+
+	/*while(1)
+	{
+		if(kbhit()) {
+			switch(getch()){
+				case 122: // z : Move forward
+					robot.avancer(100,100);
+					printf("Forward");
+					break;
+				case 115: // s : Move backward
+					robot.reculer(100,100);
+					printf("Backward");
+					break;
+				case 113: // q : Rotation to the left
+					robot.tourner_gauche(100);
+					printf("Left");
+					break;
+				case 100: // d : Rotation to the right
+					robot.tourner_droite(100);
+					printf("Right");
+					break;
+				default:
+					break;
+				}
+				flushinp();
+		}else{
+			robot.arret();
+		}
+	}*/
 }
